811c476ac9b2dd06e8626648dd7ade4c20f34e63
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-03-30 00:01:08 -0400

    implemented IteratorProvider alteration

--- src/bin.rs
+++ src/bin.rs
@@ -3,9 +3,12 @@ extern crate rust_wheels_lib;
 use rust_wheels_lib::iterators::iterator_provider::IteratorProvider;
 
 fn main() {
-    //let p = IteratorProvider::Exhaustive;
     let p = IteratorProvider::example_random();
     for i in p.u8s().take(100) {
+        println!("{}", i);
+    }
+    println!();
+    for i in p.altered("alt").u8s().take(100) {
         println!("{}", i);
     }
 }
