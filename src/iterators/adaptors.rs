dc1fd42a745367c349c325b827ed5c576ef75369
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-03-31 00:40:27 -0400

    added get_most_common_values

--- src/iterators/adaptors.rs
+++ src/iterators/adaptors.rs
@@ -1,4 +1,7 @@
+use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::fmt::Display;
+use std::hash::Hash;
 
 pub fn to_limited_string_vec<I>(xs: &mut I, size: usize) -> Vec<String>
     where I: Iterator,
@@ -22,4 +25,53 @@ pub fn to_limited_string_vec<I>(xs: &mut I, size: usize) -> Vec<String>
         }
     }
     vec
+}
+
+pub fn to_frequency_map<I>(xs: &mut I) -> HashMap<I::Item, u32>
+    where I: Iterator,
+          <I as Iterator>::Item: Eq + Hash
+{
+    let mut map: HashMap<I::Item, u32> = HashMap::new();
+    for x in xs {
+        let frequency = match map.get(&x) {
+            None => 0,
+            Some(&f) => f,
+        };
+        map.insert(x, frequency + 1);
+    }
+    map
+}
+
+pub fn get_most_common_values<I>(limit: usize, xs: &mut I) -> Vec<(I::Item, u32)>
+    where I: Iterator,
+          <I as Iterator>::Item: Clone + Eq + Hash + Display
+{
+    let map = to_frequency_map(xs);
+    let mut inverse_frequency_map: BTreeMap<u32, Vec<I::Item>> = BTreeMap::new();
+    for (x, frequency) in map {
+        let mut xs = Vec::new();
+        xs.push(x);
+        match inverse_frequency_map.get(&frequency) {
+            Some(ref ys) => xs.extend(ys.iter().cloned()),
+            _ => {}
+        };
+        inverse_frequency_map.insert(frequency, xs);
+    }
+    let mut most_common_values = Vec::new();
+    let mut i = 0;
+    for (&frequency, xs) in inverse_frequency_map.iter().rev() {
+        let mut sorted_xs = xs.clone();
+        sorted_xs.sort_by(|ref a, ref b| a.to_string().cmp(&b.to_string()));
+        for x in sorted_xs {
+            if i == limit {
+                break;
+            }
+            most_common_values.push((x, frequency));
+            i += 1;
+        }
+        if i == limit {
+            break;
+        }
+    }
+    most_common_values
 }
