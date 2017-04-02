e55c5876bd614de8f0ab27831ad09bd816381ddd
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-04-02 14:49:14 -0400

    improved demos

--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,52 @@
 extern crate rust_wheels_lib;
 
-fn main() {}
+use self::rust_wheels_lib::demos::number_utils::integer_utils_demos::*;
+use self::rust_wheels_lib::iterators::iterator_provider::IteratorProvider;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 && args.len() != 4 {
+        panic!("Usage: [exhaustive|random] [limit] [demo name]");
+    }
+    let mut exhaustive = true;
+    match args[1].as_ref() {
+        "exhaustive" => {}
+        "random" => exhaustive = false,
+        _ => panic!("Invalid first argument: {}", args[1]),
+    }
+    let p = if exhaustive {
+        IteratorProvider::Exhaustive
+    } else {
+        IteratorProvider::example_random()
+    };
+    let limit = if args.len() == 4 {
+        args[2].parse().unwrap()
+    } else {
+        usize::max_value()
+    };
+    match args[args.len() - 1].as_ref() {
+        "ceiling_log_2_u8" => demo_ceiling_log_2_u8(&p, limit),
+        "ceiling_log_2_u16" => demo_ceiling_log_2_u16(&p, limit),
+        "ceiling_log_2_u32" => demo_ceiling_log_2_u32(&p, limit),
+        "ceiling_log_2_u64" => demo_ceiling_log_2_u64(&p, limit),
+        "ceiling_log_2_usize" => demo_ceiling_log_2_usize(&p, limit),
+        "ceiling_log_2_i8" => demo_ceiling_log_2_i8(&p, limit),
+        "ceiling_log_2_i16" => demo_ceiling_log_2_i16(&p, limit),
+        "ceiling_log_2_i32" => demo_ceiling_log_2_i32(&p, limit),
+        "ceiling_log_2_i64" => demo_ceiling_log_2_i64(&p, limit),
+        "ceiling_log_2_isize" => demo_ceiling_log_2_isize(&p, limit),
+        "bits_u8" => demo_bits_u8(&p, limit),
+        "bits_u16" => demo_bits_u16(&p, limit),
+        "bits_u32" => demo_bits_u32(&p, limit),
+        "bits_u64" => demo_bits_u64(&p, limit),
+        "bits_usize" => demo_bits_usize(&p, limit),
+        "bits_i8" => demo_bits_i8(&p, limit),
+        "bits_i16" => demo_bits_i16(&p, limit),
+        "bits_i32" => demo_bits_i32(&p, limit),
+        "bits_i64" => demo_bits_i64(&p, limit),
+        "bits_isize" => demo_bits_isize(&p, limit),
+
+        _ => panic!("Invalid demo name: {}", &args[args.len() - 1]),
+    }
+}
--- src/demos/number_utils/integer_utils_demos.rs
+++ src/demos/number_utils/integer_utils_demos.rs
@@ -1,8 +1,68 @@
 use iterators::iterator_provider::IteratorProvider;
 use number_utils::integer_utils::*;
 
-pub fn demo_ceiling_log_2_u8(p: &IteratorProvider, limit: usize) {
-    for i in p.u8s().filter(|&j| j != 0).take(limit) {
-        println!("ceiling_log_2_u8({}) = {}", i, ceiling_log_2_u8(i));
+macro_rules! demo_ceiling_log_2 {
+    ($ts: expr, $d: ident, $f: ident, $pt: ident) => {
+        pub fn $d(p: &IteratorProvider, limit: usize) {
+            for i in p.$pt().take(limit) {
+                println!("ceiling_log_2_{}({}) = {}", $ts, i, $f(i));
+            }
+        }
     }
 }
+
+demo_ceiling_log_2!("u8", demo_ceiling_log_2_u8, ceiling_log_2_u8, positive_u8s);
+demo_ceiling_log_2!("u16",
+                    demo_ceiling_log_2_u16,
+                    ceiling_log_2_u16,
+                    positive_u16s);
+demo_ceiling_log_2!("u32",
+                    demo_ceiling_log_2_u32,
+                    ceiling_log_2_u32,
+                    positive_u32s);
+demo_ceiling_log_2!("u64",
+                    demo_ceiling_log_2_u64,
+                    ceiling_log_2_u64,
+                    positive_u64s);
+demo_ceiling_log_2!("usize",
+                    demo_ceiling_log_2_usize,
+                    ceiling_log_2_usize,
+                    positive_usizes);
+demo_ceiling_log_2!("i8", demo_ceiling_log_2_i8, ceiling_log_2_i8, positive_i8s);
+demo_ceiling_log_2!("i16",
+                    demo_ceiling_log_2_i16,
+                    ceiling_log_2_i16,
+                    positive_i16s);
+demo_ceiling_log_2!("i32",
+                    demo_ceiling_log_2_i32,
+                    ceiling_log_2_i32,
+                    positive_i32s);
+demo_ceiling_log_2!("i64",
+                    demo_ceiling_log_2_i64,
+                    ceiling_log_2_i64,
+                    positive_i64s);
+demo_ceiling_log_2!("isize",
+                    demo_ceiling_log_2_isize,
+                    ceiling_log_2_isize,
+                    positive_isizes);
+
+macro_rules! demo_bits {
+    ($ts: expr, $d: ident, $f: ident, $nt: ident) => {
+        pub fn $d(p: &IteratorProvider, limit: usize) {
+            for i in p.$nt().take(limit) {
+                println!("bits_{}({}) = {:?}", $ts, i, $f(i));
+            }
+        }
+    }
+}
+
+demo_bits!("u8", demo_bits_u8, bits_u8, u8s);
+demo_bits!("u16", demo_bits_u16, bits_u16, u16s);
+demo_bits!("u32", demo_bits_u32, bits_u32, u32s);
+demo_bits!("u64", demo_bits_u64, bits_u64, u64s);
+demo_bits!("usize", demo_bits_usize, bits_usize, usizes);
+demo_bits!("i8", demo_bits_i8, bits_i8, natural_i8s);
+demo_bits!("i16", demo_bits_i16, bits_i16, natural_i16s);
+demo_bits!("i32", demo_bits_i32, bits_i32, natural_i32s);
+demo_bits!("i64", demo_bits_i64, bits_i64, natural_i64s);
+demo_bits!("isize", demo_bits_isize, bits_isize, natural_isizes);
