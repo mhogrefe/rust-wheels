d3faea4988c2b648533cbf9a2c8241196f84a947
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-05-11 22:39:41 -0400

    added geometric ranges

--- src/iterators/integers_geometric.rs
+++ src/iterators/integers_geometric.rs
@@ -115,3 +115,63 @@ pub fn i32s_geometric(seed: &[u32], scale: u32) -> I32sGeometric {
         abs_gen: natural_u32s_geometric(&scramble(seed, "abs"), scale),
     }
 }
+
+pub struct RangeUpGeometricU32 {
+    naturals: NaturalU32sGeometric,
+    min: u32,
+}
+
+impl Iterator for RangeUpGeometricU32 {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        self.naturals.next().map(|u| u + self.min)
+    }
+}
+
+pub fn range_up_geometric_u32(seed: &[u32], scale: u32, min: u32) -> RangeUpGeometricU32 {
+    RangeUpGeometricU32 {
+        naturals: natural_u32s_geometric(seed, scale),
+        min: min,
+    }
+}
+
+pub struct RangeUpGeometricI32 {
+    naturals: NaturalU32sGeometric,
+    min: i32,
+}
+
+impl Iterator for RangeUpGeometricI32 {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<i32> {
+        self.naturals.next().map(|u| u as i32 + self.min)
+    }
+}
+
+pub fn range_up_geometric_i32(seed: &[u32], scale: u32, min: i32) -> RangeUpGeometricI32 {
+    RangeUpGeometricI32 {
+        naturals: natural_u32s_geometric(seed, scale),
+        min: min,
+    }
+}
+
+pub struct RangeDownGeometric {
+    naturals: NaturalU32sGeometric,
+    max: i32,
+}
+
+impl Iterator for RangeDownGeometric {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<i32> {
+        self.naturals.next().map(|u| self.max - u as i32)
+    }
+}
+
+pub fn range_down_geometric(seed: &[u32], scale: u32, max: i32) -> RangeDownGeometric {
+    RangeDownGeometric {
+        naturals: natural_u32s_geometric(seed, scale),
+        max: max,
+    }
+}
--- tests/iterators/iterator_provider_tests.rs
+++ tests/iterators/iterator_provider_tests.rs
@@ -692,16 +692,16 @@ fn test_range() {
 }
 
 #[test]
-fn test_from_vector() {
+fn test_random_from_vector() {
     let eo = get_expected_test_outputs();
-    let r_test = |number, xs| {
+    let test = |number, xs| {
         let xs: Vec<u32> = parse_vec(xs).unwrap();
         eo.match_vec_f(&format!("random_from_vector_{}", number),
                        &mut random_from_vector(&EXAMPLE_SEED[..], xs));
     };
-    r_test("i", "[5]");
-    r_test("ii", "[1, 2, 3]");
-    r_test("iii", "[3, 1, 4, 1]");
+    test("i", "[5]");
+    test("ii", "[1, 2, 3]");
+    test("iii", "[3, 1, 4, 1]");
 }
 
 #[test]
@@ -1165,4 +1165,47 @@ fn test_i32s_geometric() {
     test("iv", 3);
     test("v", 10);
     test("vi", 100);
+}
+
+#[test]
+fn test_range_up_geometric_u32() {
+    let eo = get_expected_test_outputs();
+    let test = |number, scale, min| {
+        eo.match_vec_f(&format!("random_range_up_geometric_u32_{}", number),
+                       &mut range_up_geometric_u32(&EXAMPLE_SEED[..], scale, min))
+    };
+    test("i", 0, 2);
+    test("ii", 100, 2);
+    test("iii", 0, 100);
+    test("iv", 100, 100);
+}
+
+#[test]
+fn test_range_up_geometric_i32() {
+    let eo = get_expected_test_outputs();
+    let test = |number, scale, min| {
+        eo.match_vec_f(&format!("random_range_up_geometric_i32_{}", number),
+                       &mut range_up_geometric_i32(&EXAMPLE_SEED[..], scale, min))
+    };
+    test("i", 0, 2);
+    test("ii", 100, 2);
+    test("iii", 0, 100);
+    test("iv", 100, 100);
+    test("v", 0, -100);
+    test("vi", 100, -100);
+}
+
+#[test]
+fn test_range_down_geometric() {
+    let eo = get_expected_test_outputs();
+    let test = |number, scale, min| {
+        eo.match_vec_f(&format!("random_range_down_geometric_{}", number),
+                       &mut range_down_geometric(&EXAMPLE_SEED[..], scale, min))
+    };
+    test("i", 0, 2);
+    test("ii", 100, 2);
+    test("iii", 0, 100);
+    test("iv", 100, 100);
+    test("v", 0, -100);
+    test("vi", 100, -100);
 }
