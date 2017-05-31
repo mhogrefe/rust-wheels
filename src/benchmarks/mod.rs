8663f18d0f9369d1a0ddecf2183b464419bf9ce7
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-05-30 23:30:35 -0400

    initialize benchmarks with option structs

--- src/benchmarks/mod.rs
+++ src/benchmarks/mod.rs
@@ -3,19 +3,32 @@ use stats::{mean, median};
 use std::collections::{BTreeMap, HashMap};
 use time::precise_time_ns;
 
+pub struct BenchmarkOptions2<'a, I: Iterator, IR, JITEM, JR>
+    where I::Item: 'a,
+          IR: 'a,
+          JITEM: 'a,
+          JR: 'a
+{
+    pub xs: I,
+    pub function_f: &'a Fn(I::Item) -> IR,
+    pub function_g: &'a Fn(JITEM) -> JR,
+    pub x_to_y: &'a Fn(&I::Item) -> JITEM,
+    pub x_size: &'a Fn(&I::Item) -> usize,
+    pub limit: usize,
+    pub f_name: &'a str,
+    pub g_name: &'a str,
+    pub title: &'a str,
+    pub x_axis_label: &'a str,
+    pub y_axis_label: &'a str,
+    pub file_name: &'a str,
+}
+
 #[allow(unused_variables)]
-pub fn benchmark_2<I: Iterator, IR, JITEM, JR>(xs: I,
-                                               f: &Fn(I::Item) -> IR,
-                                               g: &Fn(JITEM) -> JR,
-                                               x_to_y: &Fn(&I::Item) -> JITEM,
-                                               x_size: &Fn(&I::Item) -> usize,
-                                               limit: usize,
-                                               f_name: &str,
-                                               g_name: &str,
-                                               title: &str,
-                                               x_axis_label: &str,
-                                               y_axis_label: &str,
-                                               file_name: &str)
+pub fn benchmark_2<'a, I: Iterator, IR, JITEM, JR>(options: BenchmarkOptions2<'a,
+                                                                              I,
+                                                                              IR,
+                                                                              JITEM,
+                                                                              JR>)
     where I::Item: Clone
 {
     let reps = 100;
@@ -23,20 +36,20 @@ pub fn benchmark_2<I: Iterator, IR, JITEM, JR>(xs: I,
 
     let mut xs_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
     let mut ys_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
-    for x in xs.take(limit) {
-        let size = x_size(&x);
+    for x in options.xs.take(options.limit) {
+        let size = (options.x_size)(&x);
         let mut x_durations = Vec::new();
         let mut y_durations = Vec::new();
         for _ in 0..reps {
             let x_new = x.clone();
-            let y = x_to_y(&x);
+            let y = (options.x_to_y)(&x);
             let start_time = precise_time_ns();
-            let result = f(x_new);
+            let result = (options.function_f)(x_new);
             let end_time = precise_time_ns();
             x_durations.push(end_time - start_time);
 
             let start_time = precise_time_ns();
-            let result = g(y);
+            let result = (options.function_g)(y);
             let end_time = precise_time_ns();
             y_durations.push(end_time - start_time);
         }
@@ -65,31 +78,52 @@ pub fn benchmark_2<I: Iterator, IR, JITEM, JR>(xs: I,
     let mut fg = Figure::new();
     {
         let axes = fg.axes2d();
-        axes.set_title(title, &[]);
-        axes.set_x_label(x_axis_label, &[]);
-        axes.set_y_label(y_axis_label, &[]);
-        axes.lines(&xs_sizes, &xs_durations, &[Caption(f_name), Color("green")]);
-        axes.lines(&ys_sizes, &ys_durations, &[Caption(g_name), Color("blue")]);
+        axes.set_title(options.title, &[]);
+        axes.set_x_label(options.x_axis_label, &[]);
+        axes.set_y_label(options.y_axis_label, &[]);
+        axes.lines(&xs_sizes,
+                   &xs_durations,
+                   &[Caption(options.f_name), Color("green")]);
+        axes.lines(&ys_sizes,
+                   &ys_durations,
+                   &[Caption(options.g_name), Color("blue")]);
     }
-    fg.echo_to_file(file_name);
+    fg.echo_to_file(options.file_name);
+}
+
+pub struct BenchmarkOptions3<'a, I: Iterator, IR, JITEM, JR, KITEM, KR>
+    where I::Item: 'a,
+          IR: 'a,
+          JITEM: 'a,
+          JR: 'a,
+          KITEM: 'a,
+          KR: 'a
+{
+    pub xs: I,
+    pub function_f: &'a Fn(I::Item) -> IR,
+    pub function_g: &'a Fn(JITEM) -> JR,
+    pub function_h: &'a Fn(KITEM) -> KR,
+    pub x_to_y: &'a Fn(&I::Item) -> JITEM,
+    pub x_to_z: &'a Fn(&I::Item) -> KITEM,
+    pub x_size: &'a Fn(&I::Item) -> usize,
+    pub limit: usize,
+    pub f_name: &'a str,
+    pub g_name: &'a str,
+    pub h_name: &'a str,
+    pub title: &'a str,
+    pub x_axis_label: &'a str,
+    pub y_axis_label: &'a str,
+    pub file_name: &'a str,
 }
 
 #[allow(unused_variables)]
-pub fn benchmark_3<I: Iterator, IR, JITEM, JR, KITEM, KR>(xs: I,
-                                                          f: &Fn(I::Item) -> IR,
-                                                          g: &Fn(JITEM) -> JR,
-                                                          h: &Fn(KITEM) -> KR,
-                                                          x_to_y: &Fn(&I::Item) -> JITEM,
-                                                          x_to_z: &Fn(&I::Item) -> KITEM,
-                                                          x_size: &Fn(&I::Item) -> usize,
-                                                          limit: usize,
-                                                          f_name: &str,
-                                                          g_name: &str,
-                                                          h_name: &str,
-                                                          title: &str,
-                                                          x_axis_label: &str,
-                                                          y_axis_label: &str,
-                                                          file_name: &str)
+pub fn benchmark_3<'a, I: Iterator, IR, JITEM, JR, KITEM, KR>(options: BenchmarkOptions3<'a,
+                                                                                         I,
+                                                                                         IR,
+                                                                                         JITEM,
+                                                                                         JR,
+                                                                                         KITEM,
+                                                                                         KR>)
     where I::Item: Clone
 {
     let reps = 10;
@@ -98,27 +132,27 @@ pub fn benchmark_3<I: Iterator, IR, JITEM, JR, KITEM, KR>(xs: I,
     let mut xs_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
     let mut ys_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
     let mut zs_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
-    for x in xs.take(limit) {
-        let size = x_size(&x);
+    for x in options.xs.take(options.limit) {
+        let size = (options.x_size)(&x);
         let mut x_durations = Vec::new();
         let mut y_durations = Vec::new();
         let mut z_durations = Vec::new();
         for _ in 0..reps {
             let x_new = x.clone();
-            let y = x_to_y(&x);
-            let z = x_to_z(&x);
+            let y = (options.x_to_y)(&x);
+            let z = (options.x_to_z)(&x);
             let start_time = precise_time_ns();
-            let result = f(x_new);
+            let result = (options.function_f)(x_new);
             let end_time = precise_time_ns();
             x_durations.push(end_time - start_time);
 
             let start_time = precise_time_ns();
-            let result = g(y);
+            let result = (options.function_g)(y);
             let end_time = precise_time_ns();
             y_durations.push(end_time - start_time);
 
             let start_time = precise_time_ns();
-            let result = h(z);
+            let result = (options.function_h)(z);
             let end_time = precise_time_ns();
             z_durations.push(end_time - start_time);
         }
@@ -157,12 +191,18 @@ pub fn benchmark_3<I: Iterator, IR, JITEM, JR, KITEM, KR>(xs: I,
     let mut fg = Figure::new();
     {
         let axes = fg.axes2d();
-        axes.set_title(title, &[]);
-        axes.set_x_label(x_axis_label, &[]);
-        axes.set_y_label(y_axis_label, &[]);
-        axes.lines(&xs_sizes, &xs_durations, &[Caption(f_name), Color("green")]);
-        axes.lines(&ys_sizes, &ys_durations, &[Caption(g_name), Color("blue")]);
-        axes.lines(&zs_sizes, &zs_durations, &[Caption(h_name), Color("red")]);
+        axes.set_title(options.title, &[]);
+        axes.set_x_label(options.x_axis_label, &[]);
+        axes.set_y_label(options.y_axis_label, &[]);
+        axes.lines(&xs_sizes,
+                   &xs_durations,
+                   &[Caption(options.f_name), Color("green")]);
+        axes.lines(&ys_sizes,
+                   &ys_durations,
+                   &[Caption(options.g_name), Color("blue")]);
+        axes.lines(&zs_sizes,
+                   &zs_durations,
+                   &[Caption(options.h_name), Color("red")]);
     }
-    fg.echo_to_file(file_name);
+    fg.echo_to_file(options.file_name);
 }
