8e1aadd5cd46acc8f7365c161f745e7b54668ff3
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2017-05-31 23:47:14 -0400

    macrofy benchmarks

--- src/benchmarks/mod.rs
+++ src/benchmarks/mod.rs
@@ -3,206 +3,167 @@ use stats::{mean, median};
 use std::collections::{BTreeMap, HashMap};
 use time::precise_time_ns;
 
-pub struct BenchmarkOptions2<'a, I: Iterator, IR, JITEM, JR>
-    where I::Item: 'a,
-          IR: 'a,
-          JITEM: 'a,
-          JR: 'a
-{
-    pub xs: I,
-    pub function_f: &'a Fn(I::Item) -> IR,
-    pub function_g: &'a Fn(JITEM) -> JR,
-    pub x_to_y: &'a Fn(&I::Item) -> JITEM,
-    pub x_size: &'a Fn(&I::Item) -> usize,
-    pub limit: usize,
-    pub f_name: &'a str,
-    pub g_name: &'a str,
-    pub title: &'a str,
-    pub x_axis_label: &'a str,
-    pub y_axis_label: &'a str,
-    pub file_name: &'a str,
+macro_rules! benchmark {
+    (
+        $struct_name: ident, $fn_name: ident,
+        $(
+            [
+                $ind_type: ident,
+                $dep_type: ident,
+                $fun: ident,
+                $conv: ident,
+                $name: ident,
+                $durations_map: ident,
+                $median_durations_map: ident,
+                $durations: ident,
+                $duration: ident,
+                $elem: ident,
+                $sizes: ident,
+                $color: expr
+            ]
+        ),*
+    ) => {
+        pub struct $struct_name<'a, I: Iterator, IR, $($ind_type, $dep_type),*>
+            where I::Item: 'a, IR: 'a, $($ind_type: 'a, $dep_type: 'a),*
+        {
+            pub xs: I,
+            pub function_f: &'a Fn(I::Item) -> IR,
+            $(pub $fun: &'a Fn($ind_type) -> $dep_type),*,
+            $(pub $conv: &'a Fn(&I::Item) -> $ind_type),*,
+            pub x_param: &'a Fn(&I::Item) -> usize,
+            pub limit: usize,
+            pub f_name: &'a str,
+            $(pub $name: &'a str),*,
+            pub title: &'a str,
+            pub x_axis_label: &'a str,
+            pub y_axis_label: &'a str,
+            pub file_name: &'a str,
+        }
+
+        #[allow(unused_variables)]
+        pub fn $fn_name<I: Iterator, IR, $($ind_type, $dep_type),*>(
+            options: $struct_name<I, IR, $($ind_type, $dep_type),*>
+        )
+            where I::Item: Clone
+        {
+            let reps = 10;
+            let min_bucket_size = 5;
+
+            let mut xs_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
+            $(
+                let mut $durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
+            )*
+            for x in options.xs.take(options.limit) {
+                let size = (options.x_param)(&x);
+                let mut x_durations = Vec::new();
+                $(
+                    let mut $durations = Vec::new();
+                )*
+                for _ in 0..reps {
+                    let x_new = x.clone();
+                    let start_time = precise_time_ns();
+                    let result = (options.function_f)(x_new);
+                    let end_time = precise_time_ns();
+                    x_durations.push(end_time - start_time);
+                    $(
+                        let $elem = (options.$conv)(&x);
+                        let start_time = precise_time_ns();
+                        let result = (options.$fun)($elem);
+                        let end_time = precise_time_ns();
+                        $durations.push(end_time - start_time);
+                    )*
+                }
+                let x_duration = median(x_durations.iter().cloned()).unwrap();
+                $(
+                    let $duration = median($durations.iter().cloned()).unwrap();
+                )*
+                xs_durations_map.entry(size).or_insert_with(Vec::new).push(x_duration);
+                $(
+                    $durations_map.entry(size).or_insert_with(Vec::new).push($duration);
+                )*
+            }
+            let mut xs_median_durations_map: BTreeMap<usize, u64> = BTreeMap::new();
+            for (size, durations) in xs_durations_map {
+                if durations.len() >= min_bucket_size {
+                    xs_median_durations_map.insert(size, mean(durations.iter().cloned()) as u64);
+                }
+            }
+            $(
+                let mut $median_durations_map: BTreeMap<usize, u64> = BTreeMap::new();
+                for (size, durations) in $durations_map {
+                    if durations.len() >= min_bucket_size {
+                        $median_durations_map.insert(size, mean(durations.iter().cloned()) as u64);
+                    }
+                }
+            )*
+
+            let xs_sizes: Vec<usize> =
+                xs_median_durations_map.iter().map(|entry| *entry.0).collect();
+            let xs_durations: Vec<u64> =
+                xs_median_durations_map.iter().map(|entry| *entry.1).collect();
+            $(
+                let $sizes: Vec<usize> =
+                    $median_durations_map.iter().map(|entry| *entry.0).collect();
+                let $durations: Vec<u64> =
+                    $median_durations_map.iter().map(|entry| *entry.1).collect();
+            )*
+            let mut fg = Figure::new();
+            {
+                let axes = fg.axes2d();
+                axes.set_title(options.title, &[]);
+                axes.set_x_label(options.x_axis_label, &[]);
+                axes.set_y_label(options.y_axis_label, &[]);
+                axes.lines(&xs_sizes,
+                           &xs_durations,
+                           &[Caption(options.f_name), Color("green")]);
+                $(
+                    axes.lines(&$sizes,
+                               &$durations,
+                               &[Caption(options.$name), Color($color)]);
+                )*
+            }
+            fg.echo_to_file(options.file_name);
+        }
+    }
 }
 
-#[allow(unused_variables)]
-pub fn benchmark_2<'a, I: Iterator, IR, JITEM, JR>(options: BenchmarkOptions2<'a,
-                                                                              I,
-                                                                              IR,
-                                                                              JITEM,
-                                                                              JR>)
-    where I::Item: Clone
-{
-    let reps = 100;
-    let min_bucket_size = 10;
-
-    let mut xs_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
-    let mut ys_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
-    for x in options.xs.take(options.limit) {
-        let size = (options.x_size)(&x);
-        let mut x_durations = Vec::new();
-        let mut y_durations = Vec::new();
-        for _ in 0..reps {
-            let x_new = x.clone();
-            let y = (options.x_to_y)(&x);
-            let start_time = precise_time_ns();
-            let result = (options.function_f)(x_new);
-            let end_time = precise_time_ns();
-            x_durations.push(end_time - start_time);
-
-            let start_time = precise_time_ns();
-            let result = (options.function_g)(y);
-            let end_time = precise_time_ns();
-            y_durations.push(end_time - start_time);
-        }
-        let x_duration = median(x_durations.iter().cloned()).unwrap();
-        let y_duration = median(y_durations.iter().cloned()).unwrap();
-        xs_durations_map.entry(size).or_insert_with(Vec::new).push(x_duration);
-        ys_durations_map.entry(size).or_insert_with(Vec::new).push(y_duration);
-    }
-    let mut xs_median_durations_map: BTreeMap<usize, u64> = BTreeMap::new();
-    for (size, durations) in xs_durations_map {
-        if durations.len() >= min_bucket_size {
-            xs_median_durations_map.insert(size, mean(durations.iter().cloned()) as u64);
-        }
-    }
-    let mut ys_median_durations_map: BTreeMap<usize, u64> = BTreeMap::new();
-    for (size, durations) in ys_durations_map {
-        if durations.len() >= min_bucket_size {
-            ys_median_durations_map.insert(size, mean(durations.iter().cloned()) as u64);
-        }
-    }
-
-    let xs_sizes: Vec<usize> = xs_median_durations_map.iter().map(|entry| *entry.0).collect();
-    let xs_durations: Vec<u64> = xs_median_durations_map.iter().map(|entry| *entry.1).collect();
-    let ys_sizes: Vec<usize> = ys_median_durations_map.iter().map(|entry| *entry.0).collect();
-    let ys_durations: Vec<u64> = ys_median_durations_map.iter().map(|entry| *entry.1).collect();
-    let mut fg = Figure::new();
-    {
-        let axes = fg.axes2d();
-        axes.set_title(options.title, &[]);
-        axes.set_x_label(options.x_axis_label, &[]);
-        axes.set_y_label(options.y_axis_label, &[]);
-        axes.lines(&xs_sizes,
-                   &xs_durations,
-                   &[Caption(options.f_name), Color("green")]);
-        axes.lines(&ys_sizes,
-                   &ys_durations,
-                   &[Caption(options.g_name), Color("blue")]);
-    }
-    fg.echo_to_file(options.file_name);
-}
-
-pub struct BenchmarkOptions3<'a, I: Iterator, IR, JITEM, JR, KITEM, KR>
-    where I::Item: 'a,
-          IR: 'a,
-          JITEM: 'a,
-          JR: 'a,
-          KITEM: 'a,
-          KR: 'a
-{
-    pub xs: I,
-    pub function_f: &'a Fn(I::Item) -> IR,
-    pub function_g: &'a Fn(JITEM) -> JR,
-    pub function_h: &'a Fn(KITEM) -> KR,
-    pub x_to_y: &'a Fn(&I::Item) -> JITEM,
-    pub x_to_z: &'a Fn(&I::Item) -> KITEM,
-    pub x_size: &'a Fn(&I::Item) -> usize,
-    pub limit: usize,
-    pub f_name: &'a str,
-    pub g_name: &'a str,
-    pub h_name: &'a str,
-    pub title: &'a str,
-    pub x_axis_label: &'a str,
-    pub y_axis_label: &'a str,
-    pub file_name: &'a str,
-}
-
-#[allow(unused_variables)]
-pub fn benchmark_3<'a, I: Iterator, IR, JITEM, JR, KITEM, KR>(options: BenchmarkOptions3<'a,
-                                                                                         I,
-                                                                                         IR,
-                                                                                         JITEM,
-                                                                                         JR,
-                                                                                         KITEM,
-                                                                                         KR>)
-    where I::Item: Clone
-{
-    let reps = 10;
-    let min_bucket_size = 10;
-
-    let mut xs_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
-    let mut ys_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
-    let mut zs_durations_map: HashMap<usize, Vec<f64>> = HashMap::new();
-    for x in options.xs.take(options.limit) {
-        let size = (options.x_size)(&x);
-        let mut x_durations = Vec::new();
-        let mut y_durations = Vec::new();
-        let mut z_durations = Vec::new();
-        for _ in 0..reps {
-            let x_new = x.clone();
-            let y = (options.x_to_y)(&x);
-            let z = (options.x_to_z)(&x);
-            let start_time = precise_time_ns();
-            let result = (options.function_f)(x_new);
-            let end_time = precise_time_ns();
-            x_durations.push(end_time - start_time);
-
-            let start_time = precise_time_ns();
-            let result = (options.function_g)(y);
-            let end_time = precise_time_ns();
-            y_durations.push(end_time - start_time);
-
-            let start_time = precise_time_ns();
-            let result = (options.function_h)(z);
-            let end_time = precise_time_ns();
-            z_durations.push(end_time - start_time);
-        }
-        let x_duration = median(x_durations.iter().cloned()).unwrap();
-        let y_duration = median(y_durations.iter().cloned()).unwrap();
-        let z_duration = median(z_durations.iter().cloned()).unwrap();
-        xs_durations_map.entry(size).or_insert_with(Vec::new).push(x_duration);
-        ys_durations_map.entry(size).or_insert_with(Vec::new).push(y_duration);
-        zs_durations_map.entry(size).or_insert_with(Vec::new).push(z_duration);
-    }
-    let mut xs_median_durations_map: BTreeMap<usize, u64> = BTreeMap::new();
-    for (size, durations) in xs_durations_map {
-        if durations.len() >= min_bucket_size {
-            xs_median_durations_map.insert(size, mean(durations.iter().cloned()) as u64);
-        }
-    }
-    let mut ys_median_durations_map: BTreeMap<usize, u64> = BTreeMap::new();
-    for (size, durations) in ys_durations_map {
-        if durations.len() >= min_bucket_size {
-            ys_median_durations_map.insert(size, mean(durations.iter().cloned()) as u64);
-        }
-    }
-    let mut zs_median_durations_map: BTreeMap<usize, u64> = BTreeMap::new();
-    for (size, durations) in zs_durations_map {
-        if durations.len() >= min_bucket_size {
-            zs_median_durations_map.insert(size, mean(durations.iter().cloned()) as u64);
-        }
-    }
-
-    let xs_sizes: Vec<usize> = xs_median_durations_map.iter().map(|entry| *entry.0).collect();
-    let xs_durations: Vec<u64> = xs_median_durations_map.iter().map(|entry| *entry.1).collect();
-    let ys_sizes: Vec<usize> = ys_median_durations_map.iter().map(|entry| *entry.0).collect();
-    let ys_durations: Vec<u64> = ys_median_durations_map.iter().map(|entry| *entry.1).collect();
-    let zs_sizes: Vec<usize> = zs_median_durations_map.iter().map(|entry| *entry.0).collect();
-    let zs_durations: Vec<u64> = zs_median_durations_map.iter().map(|entry| *entry.1).collect();
-    let mut fg = Figure::new();
-    {
-        let axes = fg.axes2d();
-        axes.set_title(options.title, &[]);
-        axes.set_x_label(options.x_axis_label, &[]);
-        axes.set_y_label(options.y_axis_label, &[]);
-        axes.lines(&xs_sizes,
-                   &xs_durations,
-                   &[Caption(options.f_name), Color("green")]);
-        axes.lines(&ys_sizes,
-                   &ys_durations,
-                   &[Caption(options.g_name), Color("blue")]);
-        axes.lines(&zs_sizes,
-                   &zs_durations,
-                   &[Caption(options.h_name), Color("red")]);
-    }
-    fg.echo_to_file(options.file_name);
-}
+benchmark!(BenchmarkOptions2,
+           benchmark_2,
+           [JITEM,
+            JR,
+            function_g,
+            x_to_y,
+            g_name,
+            ys_durations_map,
+            ys_median_durations_map,
+            ys_durations,
+            y_duration,
+            y,
+            y_sizes,
+            "blue"]);
+benchmark!(BenchmarkOptions3,
+           benchmark_3,
+           [JITEM,
+            JR,
+            function_g,
+            x_to_y,
+            g_name,
+            ys_durations_map,
+            ys_median_durations_map,
+            ys_durations,
+            y_duration,
+            y,
+            y_sizes,
+            "blue"],
+           [KITEM,
+            KR,
+            function_h,
+            x_to_z,
+            h_name,
+            zs_durations_map,
+            zs_median_durations_map,
+            zs_durations,
+            z_duration,
+            z,
+            z_sizes,
+            "red"]);
