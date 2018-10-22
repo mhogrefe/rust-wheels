ba703d5317b3c3aa287668fe8ff36811cee11614
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2018-10-21 22:06:29 -0400

    updated dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-#![allow(
-    unknown_lints,
-    suspicious_arithmetic_impl,
-    suspicious_op_assign_impl
-)]
+#![allow(unknown_lints, suspicious_arithmetic_impl, suspicious_op_assign_impl)]
 
 extern crate gnuplot;
 extern crate itertools;
