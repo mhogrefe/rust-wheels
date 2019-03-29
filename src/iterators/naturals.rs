90c722b0cfa991563b0b53220a656fd1c4560f8f
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2019-03-29 00:58:56 -0400

    added some special random Natural ranges

--- src/iterators/naturals.rs
+++ src/iterators/naturals.rs
@@ -6,6 +6,7 @@ use iterators::integers_geometric::{
 use malachite_base::num::{One, SignificantBits, Zero};
 use malachite_nz::natural::random::random_natural_below::random_natural_below;
 use malachite_nz::natural::random::random_natural_with_bits::random_natural_with_bits;
+use malachite_nz::natural::random::special_random_natural_below::*;
 use malachite_nz::natural::random::special_random_natural_with_bits::*;
 use malachite_nz::natural::Natural;
 use rand::{IsaacRng, SeedableRng};
@@ -152,6 +153,35 @@ pub fn random_range_natural(seed: &[u32], a: Natural, b: Natural) -> RandomRange
     }
 }
 
+pub struct SpecialRandomRangeNatural {
+    rng: Box<IsaacRng>,
+    diameter_plus_one: Natural,
+    a: Natural,
+}
+
+impl Iterator for SpecialRandomRangeNatural {
+    type Item = Natural;
+
+    fn next(&mut self) -> Option<Natural> {
+        Some(special_random_natural_below(&mut self.rng, &self.diameter_plus_one) + &self.a)
+    }
+}
+
+pub fn special_random_range_natural(
+    seed: &[u32],
+    a: Natural,
+    b: Natural,
+) -> SpecialRandomRangeNatural {
+    if a > b {
+        panic!("a must be less than or equal to b. a: {}, b: {}", a, b);
+    }
+    SpecialRandomRangeNatural {
+        rng: Box::new(IsaacRng::from_seed(seed)),
+        diameter_plus_one: b - &a + 1,
+        a,
+    }
+}
+
 pub struct RandomRangeUpNatural {
     rng: Box<IsaacRng>,
     bit_sizes: RangeUpGeometricU32,
@@ -182,6 +212,48 @@ pub fn random_range_up_natural(seed: &[u32], scale: u32, a: Natural) -> RandomRa
     // a - 2^(n - 1) are smaller than a, so 2^(n - 1) - (a - 2^(n - 1)) = 2^n - a are at least a.
     let offset_limit = (Natural::ONE << a_bit_size) - &a;
     RandomRangeUpNatural {
+        rng: Box::new(IsaacRng::from_seed(&scramble(seed, "bits"))),
+        bit_sizes: range_up_geometric_u32(&scramble(seed, "bitsizes"), scale, a_bit_size as u32),
+        a,
+        a_bit_size,
+        offset_limit,
+    }
+}
+
+pub struct SpecialRandomRangeUpNatural {
+    rng: Box<IsaacRng>,
+    bit_sizes: RangeUpGeometricU32,
+    a: Natural,
+    a_bit_size: u64,
+    offset_limit: Natural,
+}
+
+impl Iterator for SpecialRandomRangeUpNatural {
+    type Item = Natural;
+
+    fn next(&mut self) -> Option<Natural> {
+        let bit_size = u64::from(self.bit_sizes.next().unwrap());
+        Some(if bit_size == self.a_bit_size {
+            // Generates values between a and 2^n - 1, inclusive.
+            special_random_natural_below(&mut self.rng, &self.offset_limit) + &self.a
+        } else {
+            // Generates values between 2^(n - 1) and 2^n - 1, inclusive.
+            special_random_natural_with_bits(&mut self.rng, bit_size)
+        })
+    }
+}
+
+pub fn special_random_range_up_natural(
+    seed: &[u32],
+    scale: u32,
+    a: Natural,
+) -> SpecialRandomRangeUpNatural {
+    let a_bit_size = a.significant_bits();
+    // let n = a.significant_bits().
+    // There are 2^(n - 1) values with exactly n bits, the smallest being 2^(n - 1);
+    // a - 2^(n - 1) are smaller than a, so 2^(n - 1) - (a - 2^(n - 1)) = 2^n - a are at least a.
+    let offset_limit = (Natural::ONE << a_bit_size) - &a;
+    SpecialRandomRangeUpNatural {
         rng: Box::new(IsaacRng::from_seed(&scramble(seed, "bits"))),
         bit_sizes: range_up_geometric_u32(&scramble(seed, "bitsizes"), scale, a_bit_size as u32),
         a,
