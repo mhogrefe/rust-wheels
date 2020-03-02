84d97d5aea24cdfa00a07849477da4fc5431e761
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2020-03-02 04:44:31 -0500

    cleaned up vec parsing a bit

--- src/io/readers.rs
+++ src/io/readers.rs
@@ -1,23 +1,20 @@
 use std::str::FromStr;
 
-#[allow(unused_assignments)]
 pub fn parse_vec<T: FromStr>(s: &str) -> Option<Vec<T>> {
     if s.is_empty() {
         return None;
     }
-    let mut cleaned_first_token = String::new();
-    let mut cleaned_last_token = String::new();
     let mut tokens = s.split(", ").collect::<Vec<&str>>();
     let last_token_index = tokens.len() - 1;
     if tokens[0].is_empty() || tokens[last_token_index].is_empty() {
         return None;
     }
-    cleaned_first_token = String::from(tokens[0]);
+    let mut cleaned_first_token = String::from(tokens[0]);
     if cleaned_first_token.remove(0) != '[' {
         return None;
     }
     tokens[0] = &cleaned_first_token;
-    cleaned_last_token = String::from(tokens[last_token_index]);
+    let mut cleaned_last_token = String::from(tokens[last_token_index]);
     if cleaned_last_token.pop().unwrap() != ']' {
         return None;
     }
