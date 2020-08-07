d9a330d7f27604f103d987dd53ab9daa04858241
Mikhail Hogrefe <mikhailhogrefe@gmail.com>
2020-08-07 04:27:11 -0400

    use new range function

--- src/iterators/vecs.rs
+++ src/iterators/vecs.rs
@@ -3,8 +3,7 @@ use std::marker::PhantomData;
 
 use malachite_base::num::basic::unsigneds::PrimitiveUnsigned;
 use malachite_base::num::conversion::traits::{ExactFrom, WrappingFrom};
-use malachite_base::num::exhaustive::exhaustive_positive_primitives;
-use malachite_base::num::exhaustive::primitive_integer_increasing_range_to_max;
+use malachite_base::num::exhaustive::{exhaustive_positive_primitives, exhaustive_unsigneds};
 use malachite_base::num::logic::traits::BitConvertible;
 use malachite_nz::natural::random::special_random_natural_up_to_bits::*;
 use malachite_nz::natural::random::special_random_natural_with_bits::*;
@@ -224,10 +223,9 @@ pub fn exhaustive_vecs_shortlex<I: 'static + Clone + Iterator>(
 where
     I::Item: Clone,
 {
-    Box::new(Concat::new(
-        primitive_integer_increasing_range_to_max(0)
-            .map(move |i| exhaustive_fixed_size_vecs_from_single(i, xs.clone())),
-    ))
+    Box::new(Concat::new(exhaustive_unsigneds().map(move |i| {
+        exhaustive_fixed_size_vecs_from_single(i, xs.clone())
+    })))
 }
 
 pub struct RandomFixedLengthVecs<I: Iterator> {
